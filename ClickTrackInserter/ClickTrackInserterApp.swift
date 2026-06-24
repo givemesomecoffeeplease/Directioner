3bbe3bc2cce4012a05f13dba153cabb5d274704b
hanhee5676 <hanhee5676@gmail.com>
2026-06-25 02:21:14 +0900

    Initial Commit

---
+++ ClickTrackInserter/ClickTrackInserterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickTrackInserterApp.swift
+//  ClickTrackInserter
+//
+//  Created by 한희 on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ClickTrackInserterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickTrackInserter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClickTrackInserter
+//
+//  Created by 한희 on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
